2305aae904838aac35a6d412570f560de20a7173
Nivethaa-Sakthivel <nivi5462@gmail.com>
2025-07-12 10:33:12 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,16 @@
+ public class main{
+     
+          int marks;
+          String name;
+      
+      Student(){
+          System.out.println("Heyyy");
+      }
+      public static void main(String[] args){
+          main obj=new main();
+          main obj1=new main();
+          System.out.println(obj.marks);
+          System.out.println(obj1.name);
+          
+      }
+  }
