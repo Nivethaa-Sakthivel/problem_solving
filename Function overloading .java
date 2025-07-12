0b52a2223b121df06beb4a8da19da463564e2832
Nivethaa-Sakthivel <nivi5462@gmail.com>
2025-07-12 10:18:45 +0530

    Create Function overloading .java

---
+++ Function overloading .java
@@ -0,0 +1,12 @@
+
+ public class Main{
+     void Baby(int a,int b){
+       System.out.println(a+b);
+   }
+    public static void main(String[] args){
+        Main main=new Main();
+       main.Baby(20,30);
+    }
+}
+
+
