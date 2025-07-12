8ee17a4b8499ac48350bcd2c204be1288e0cde1c
Nivethaa-Sakthivel <nivi5462@gmail.com>
2025-07-12 10:19:11 +0530

    Create For-each loop.java

---
+++ For-each loop.java
@@ -0,0 +1,10 @@
+
+
+public class main{
+    public static void main(String[] args){
+        int num[]={1,2,3,4,5,6,7};
+        for(int var:num){
+            System.out.println(var);
+        }
+    }
+}
