d12d468f266a6312885377131040c4d3ae87dd32
Nivethaa-Sakthivel <nivi5462@gmail.com>
2025-07-12 10:40:03 +0530

    Update and rename constructor.java to constructor and with parameter.java

--- constructor and with parameter.java
+++ constructor and with parameter.java
@@ -1,17 +1,20 @@
 
+ 
   public class main{
      
           int marks;
           String name;
-      
-      main(){
+       main(){
+          System.out.println("Hellowww");
+      }
+      main(int a){
+           System.out.println(a);
           System.out.println("Heyyy");
       }
       public static void main(String[] args){
-          main obj=new main();
+          main obj=new main(20);
           main obj1=new main();
-          System.out.println(obj.marks);
-          System.out.println(obj1.name);
+          
           
       }
   }
